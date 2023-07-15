0707a317d8a2fb6281a7147e69323c0f4c36fce9
amyhadassah <amyhadassahbb@gmail.com>
2023-07-14 19:57:40 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int i = '0';
+int j = '0';
+while (i <= '8')
+{
+while (j <= '9')
+{
+if (i < j)
+{
+putchar(i);
+putchar(j);
+if (!(i == '8' && j == '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+j++;
+}
+j = ++i;
+}
+putchar('\n');
+return (0);
+}
