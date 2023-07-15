29dfd48725dbd368157d26167219919fda468c90
amyhadassah <amyhadassahbb@gmail.com>
2023-07-14 20:46:39 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+
 /**
-main - main funtion
-*
-* Return: always 0
-*/
+ * main - main function
+ *
+ * Return: alwasy 0
+ */
+
 int main(void)
 {
-int i;
-char j;
-for (i = 0 ; i < 10 ; i++)
-putchar(i + '0');
-for (j = 'a' ; j <= 'f' ; j++)
-putchar(j);
-putchar('\n');
-return (0);
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
 }
