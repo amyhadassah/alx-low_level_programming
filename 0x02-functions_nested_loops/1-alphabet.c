d6923c4af765f19314508d84e777ba581b0b38b2
amyhadassah <amyhadassahbb@gmail.com>
2023-07-18 15:50:30 -0700

    Task 0 Main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print the alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
