5a2b58317d92d238df59a79e274a0243c825a2a6
amyhadassah <amyhadassahbb@gmail.com>
2023-07-27 10:02:12 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+void execute_add(char *n, char *m, char *r, int size)
+{
+	char *res;
+	res = infinite_add(n, m, r, size);
+	if (res == 0)
+		printf("Error\n");
+	else
+		printf("%s + %s = %s\n", n, m, res);
+}
+
 /**
  * main - check the code
  *
@@ -12,19 +22,18 @@ int main(void)
 	char *n2 = "734563456453743756756784458";
 	char *m1 = "9034790663470697234682914569346259634958693246597324659762347";
 	char *m2 = "956349265983465962349569346";
-	char r[100], r2[10], r3[11], *n = "1234567890", *m = "1", *n3 = "999999999", *m3 = "1";
-	char *res;
+	char r[100], r2[10], r3[11];
+	char *n = "1234567890";
+	char *m = "1";
+	char *n3 = "999999999";
+	char *m3 = "1";
 
-	res = infinite_add(n1, m1, r, 100);
-	res == 0 ? printf("Error\n") : printf("%s + %s = %s\n", n1, m1, res);
-	res = infinite_add(n2, m2, r, 100);
-	res == 0 ? printf("Error\n") : printf("%s + %s = %s\n", n2, m2, res);
-	res = infinite_add(n, m, r2, 10);
-	res == 0 ? printf("Error\n") : printf("%s + %s = %s\n", n, m, res);
-	res = infinite_add(n3, m3, r2, 10);
-	res == 0 ? printf("Error\n") : printf("%s + %s = %s\n", n3, m3, res);
-	res = infinite_add(n3, m3, r3, 11);
-	res == 0 ? printf("Error\n") : printf("%s + %s = %s\n", n3, m3, res);
+	execute_add(n1, m1, r, 100);
+	execute_add(n2, m2, r, 100);
+	execute_add(n, m, r2, 10);
+	execute_add(n3, m3, r2, 10);
+	execute_add(n3, m3, r3, 11);
+
 	return (0);
 }
 
