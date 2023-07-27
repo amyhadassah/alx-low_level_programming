b234442943473488056dcb165307893cf44ca298
amyhadassah <amyhadassahbb@gmail.com>
2023-07-27 10:04:12 -0700

    Task Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,5 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code for
- *
- * Return: Always 0.
- */
 int main(void)
 {
 	int n;
@@ -14,7 +8,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	*(p + 2) = 98; /* Add this line to set a[2] to 98 */
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
