b28406156d6a520e37d18693cdd0673797c3580b
amyhadassah <amyhadassahbb@gmail.com>
2023-08-04 12:13:04 -0700

    nargs

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed to it
- * @argc: the number of arguments passed to the program
- * @argv: an array of pointers to the arguments
- *
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: Array of character pointers listing all the arguments
+ * 
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
-	printf("Program name: %s\n", argv[0]);
+	int num_arguments; /* Moved this declaration to the start of the block */
+
+	(void)argv; /* This line is to avoid the unused parameter error */
+
+	num_arguments = argc - 1; /* Now this is not a declaration, but an assignment */
+	printf("%d\n", num_arguments);
+
 	return (0);
 }
