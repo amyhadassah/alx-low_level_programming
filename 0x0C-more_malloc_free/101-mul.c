564e33c2c95938549ac56950e25fa8efd6b56b4f
amyhadassah <amyhadassahbb@gmail.com>
2023-08-10 15:45:08 -0700

    Mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * is_digit - check if a string only contains digits
+ * @s: string to check
+ * Return: 1 if true, 0 otherwise
+ */
 int is_digit(char *s)
 {
 	int i = 0;
 
-	if (s[0] == '+')  /* Allow numbers to optionally start with a '+' */
-		i = 1;
+	if (!s)
+		return (0);
 
-	for (; s[i]; i++)
+	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
+		i++;
 	}
 	return (1);
 }
 
+/**
+ * main - multiplies two positive numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 98 on error
+ */
 int main(int argc, char *argv[])
 {
 	long num1, num2;
@@ -34,6 +46,7 @@ int main(int argc, char *argv[])
 
 	num1 = atol(argv[1]);
 	num2 = atol(argv[2]);
+
 	printf("%ld\n", num1 * num2);
 	return (0);
 }
