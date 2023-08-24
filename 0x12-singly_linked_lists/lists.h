a13fae29ea6721301faf6b0adce0c4af664f7391
amyhadassah <amyhadassahbb@gmail.com>
2023-08-24 11:12:06 -0700

    updated singly list task

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,28 @@
-#ifndef "LISTS_H"
-#define "LISTS_H"
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stddef.h>  /* Include this for size_t definition */
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
+/* Additional function prototypes and other definitions */
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
