c63e0535aa55a87b68af74e57edeb893bca6719b
amyhadassah <amyhadassahbb@gmail.com>
2023-08-29 12:26:34 -0700

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,47 +1,35 @@
+
 #include "lists.h"
 #include <stdlib.h>
 
 /**
- * insert_node - Helper function to actually insert the node.
- * @temp: The node after which the new node should be inserted.
- * @n: The data for the new node.
- * Return: The new node or NULL if it failed.
- */
-listint_t *insert_node(listint_t *temp, int n)
-{
-	listint_t *new_node;
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
-}
-
-/**
- * insert_nodeint_at_index - Inserts a new node at a given position.
- * @head: Double pointer to the head of the list.
- * @idx: The index at which to insert the new node, starting at 0.
- * @n: The data for the new node.
- * Return: The address of the new node, or NULL if it failed.
+ * insert_nodeint_at_index - Inserts a new node at a given position
+ * @head: Pointer to a pointer to the head of the list
+ * @idx: Index at which to insert the new node
+ * @n: Data to store in the new node
+ * 
+ * Return: Address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp;
 	unsigned int i;
+	listint_t *new_node, *temp;
 
 	if (head == NULL)
-	{
 		return (NULL);
-	}
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
-		return (insert_node(*head, n));
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 
 	temp = *head;
@@ -49,6 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (temp == NULL)
 		{
+			free(new_node);
 			return (NULL);
 		}
 		temp = temp->next;
@@ -56,9 +45,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (temp == NULL)
 	{
+		free(new_node);
 		return (NULL);
 	}
 
-	return (insert_node(temp, n));
+	new_node->next = temp->next;
+	temp->next = new_node;
+
+	return (new_node);
 }
 
