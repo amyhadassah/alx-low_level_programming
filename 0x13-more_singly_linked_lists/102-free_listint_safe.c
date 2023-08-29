1108e9934bdbdd9de3badbb02bcb89406766c034
amyhadassah <amyhadassahbb@gmail.com>
2023-08-29 14:26:23 -0700

    changes

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,50 +1,36 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - Safely frees a listint_t list.
- * @h: A double pointer to the head of the list.
+ * free_listint_safe - Frees a listint_t linked list safely.
+ * @h: Pointer to the head of the linked list.
  *
- * Return: The size of the list that was free'd.
+ * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t count = 0;
-	listint_t *slow = *h, *fast = *h, *tmp;
+	listint_t *current, *temp;
 
-	if (h == NULL || *h == NULL)
+	if (h == NULL)
 		return (0);
 
-	/* Detect loop using tortoise and hare algorithm */
-	while (fast && fast->next)
+	current = *h;
+
+	while (current != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)  // Loop detected
-		{
-			slow = *h;
-
-			while (slow != fast)
-			{
-				slow = slow->next;
-				fast = fast->next;
-			}
-
-			/* Break the loop */
-			fast->next = NULL;
-		}
-	}
-
-	/* Free the list */
-	while (*h)
-	{
-		tmp = (*h)->next;
-		free(*h);
-		*h = tmp;
 		count++;
+		temp = current;
+		current = current->next;
+
+		temp->next = NULL;
+		free(temp);
+
+		if (temp <= current)
+			break;
 	}
 
 	*h = NULL;
+
 	return (count);
 }
-
