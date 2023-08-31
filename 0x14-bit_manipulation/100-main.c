327783956bf1a81b2b0dbf3bded6d6386dd7646a
amyhadassah <amyhadassahbb@gmail.com>
2023-08-31 16:43:35 -0700

    changes

--- 0x14-bit_manipulation/100-main.c
+++ 0x14-bit_manipulation/100-main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Determines the endianness of the system and prints
+ *              an appropriate message indicating whether it is little
+ *              endian or big endian.
+ *
+ * Return: 0 on successful execution
+ */
+
 int main(void)
 {
 	int n;
