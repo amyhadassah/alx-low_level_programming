f0bd1f236b7844127d1a04ef979d3b05c161df95
Amy Hadassah <amyhadassahbb@gmail.com>
2023-11-27 16:02:00 -0800

    changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,16 +5,13 @@
 
 #define MAXSIZE 1024
 
-void handle_errors(int error_code, char *file_name, int fd);
-
-int open_file(char *filename, int flags, mode_t mode);
-void close_file(int fd);
+void error_exit(int error_code, char *file_name, int fd);
 
 /**
- * main - Copies the content of one file to another.
- * @argc: Number of arguments.
- * @argv: Arguments vector.
- * Return: 0 on success, or a specific error code on failure.
+ * main - Copies the content of one file to another file.
+ * @argc: The number of command-line arguments.
+ * @argv: The command-line arguments.
+ * Return: 0 on success, or an error code on failure.
  */
 int main(int argc, char *argv[])
 {
@@ -23,34 +20,44 @@ int main(int argc, char *argv[])
 	char buffer[MAXSIZE];
 
 	if (argc != 3)
-		handle_errors(97, NULL, 0);
+		error_exit(97, NULL, 0);
 
-	file_in = open_file(argv[1], O_RDONLY, 0);
-	file_out = open_file(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	file_in = open(argv[1], O_RDONLY);
+	if (file_in == -1)
+		error_exit(98, argv[1], 0);
+
+	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_out == -1)
+		error_exit(99, argv[2], 0);
 
 	while ((read_stat = read(file_in, buffer, MAXSIZE)) > 0)
 	{
 		write_stat = write(file_out, buffer, read_stat);
 		if (write_stat != read_stat)
-			handle_errors(99, argv[2], 0);
+			error_exit(99, argv[2], 0);
 	}
 
-	if (read_stat < 0)
-		handle_errors(98, argv[1], 0);
+	if (read_stat == -1)
+		error_exit(98, argv[1], 0);
 
-	close_file(file_in);
-	close_file(file_out);
+	if (close(file_in) == -1)
+		error_exit(100, NULL, file_in);
+
+	if (close(file_out) == -1)
+		error_exit(100, NULL, file_out);
 
 	return (0);
 }
 
 /**
- * handle_errors - Prints error messages based on the error code and exits.
- * @error_code: The error code.
- * @file_name: The name of the file causing the error.
- * @fd: The file descriptor causing the error.
+ * error_exit - Handle errors by printing appropriate
+ *  error messages and exiting.
+ * @error_code: The error code that determines the error message.
+ * @file_name: The file name that the error refers to.
+ * @fd: The file descriptor that the error refers to.
  */
-void handle_errors(int error_code, char *file_name, int fd)
+
+void error_exit(int error_code, char *file_name, int fd)
 {
 	switch (error_code)
 	{
@@ -70,36 +77,5 @@ void handle_errors(int error_code, char *file_name, int fd)
 			return;
 	}
 	exit(error_code);
-}
-
-/**
- * open_file - Opens a file and handles errors.
- * @filename: Name of the file.
- * @flags: Flags for opening the file.
- * @mode: Mode for the file if it needs to be created.
- * Return: File descriptor.
- */
-int open_file(char *filename, int flags, mode_t mode)
-{
-	int fd = open(filename, flags, mode);
-
-	if (fd < 0)
-	{
-		if (flags & O_RDONLY)
-			handle_errors(98, filename, 0);
-		else
-			handle_errors(99, filename, 0);
-	}
-	return (fd);
-}
-
-/**
- * close_file - Closes a file and handles errors.
- * @fd: File descriptor.
- */
-void close_file(int fd)
-{
-	if (close(fd) < 0)
-		handle_errors(100, NULL, fd);
 }
 
