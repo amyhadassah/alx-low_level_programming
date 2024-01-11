e3eac911330ba36a4d46fc99bb494cef215123e0
Amy Hadassah <amyhadassahbb@gmail.com>
2024-01-11 10:30:27 -0800

    Task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,8 @@
  * @head: pointer to the head of the doubly linked list
  * @index: index of the node, starting from 0
  *
- * Return: the nth node of a dlistint_t linked list, or NULL if it doesn't exist
+ * Return: the nth node of a dlistint_t
+ * linked list, or NULL if it doesn't exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
